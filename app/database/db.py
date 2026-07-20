7a3d9670db7c12ec231261b5716a01849cef519d
Lev <stetsyuk.lev@yandex.ru>
2026-07-20 19:16:56 +0300

    Version 2.0

--- app/database/db.py
+++ app/database/db.py
@@ -1,16 +1,30 @@
 import sqlite3
 from pathlib import Path
+import sys
 
-DB_PATH = Path("data/writeoffs.db")
+
+if getattr(sys, "frozen", False):
+    BASE_DIR = Path(sys.executable).parent
+else:
+    BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+DATA_DIR = BASE_DIR / "data"
+DATA_DIR.mkdir(exist_ok=True)
+
+DB_PATH = DATA_DIR / "writeoffs.db"
 
 
 def get_connection():
-    conn = sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(
+        DB_PATH,
+        timeout=30,
+        check_same_thread=False
+    )
 
-    # Позволяет обращаться к столбцам по имени
     conn.row_factory = sqlite3.Row
 
-    # Включаем поддержку внешних ключей
     conn.execute("PRAGMA foreign_keys = ON")
+    conn.execute("PRAGMA busy_timeout = 30000")
 
     return conn
